3a05b39cac6019d7bad24df966696d8919868d13
NeejWeej <nk6ce@virginia.edu>
2021-05-31 20:49:47 -0400

    collapse workouts

--- scripts.js
+++ scripts.js
@@ -5,3 +5,17 @@ searchButton.addEventListener('click', () => {
   var searchResults = document.getElementById('get_big_fast_result');
   searchResults.innerHTML = "<h3>Search results for '" + inputValue + "'</h3>";
 });
+var coll = document.getElementsByClassName("collapsible");
+var i;
+// below is for collapsing different workouts
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+};
