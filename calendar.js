09ef8787f43b7f8d2b917234d4c0d43dc45ebe02
dyld-w <dd4cnj@virginia.edu>
2021-06-02 00:49:21 -0400

    schedule w/ to do and calendar

--- calendar.js
+++ calendar.js
@@ -1,126 +1,3 @@
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '821150405737-c7jbahbgdk3a2nq00o84po7ip8i7568l.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBm_XnhkRSPIj4cUNU5PsYS8iWNRj27CC0';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar";
-
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    listUpcomingEvents();
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-    var len = 0;
-  gapi.client.calendar.events.list({
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime'
-  }).then(function(response) {
-    var events = response.result.items;
-    appendPre('Upcoming events:');
-
-    if (events.length > 0) {
-        len = events.length;
-      for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        var when = event.start.dateTime;
-        if (!when) {
-          when = event.start.date;
-        }
-        if (event.summary.includes("Workout: ")){
-            appendPre(event.summary + ' (' + when + ')')
-
-        }
-      }
-    } else {
-      appendPre('No upcoming events found.');
-    }
-  });
-  return len;
-}
-
 YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
     
     // Create a new instance of calendar, placing it in
@@ -150,7 +27,6 @@ YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
         // Format the date and output it to a DOM
         // element.
         Y.one("#selecteddate").setHTML(dtdate.format(newDate));
-        document.getElementById("workoutOnDate").innerHTML = listUpcomingEvents();
     });
 
 
@@ -170,3 +46,93 @@ YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
         calendar.set('showNextMonth', !(calendar.get("showNextMonth")));
     });
 });
+
+document.getElementById("taskdesc").addEventListener("focus", validateWorkoutTitle);
+document.getElementById("duedate").addEventListener("focus", validateTaskDesc);
+function validateWorkoutTitle() 
+{
+   if (document.getElementById("workoutdesc").value === '')
+   {
+      document.getElementById("workoutdesc").focus();
+      document.getElementById("workout_error").innerHTML = "Please enter workout title";
+   }
+   else 
+    document.getElementById("workout_error").innerHTML = "";
+}
+
+function validateTaskDesc() 
+{
+   if (document.getElementById("taskdesc").value === '')
+   {
+      document.getElementById("taskdesc").focus();
+      document.getElementById("taskdesc-note").innerHTML = "Please enter task description";
+   }
+   else 
+    document.getElementById("taskdesc-note").innerHTML = "";
+}
+
+
+function addRow()
+{	  
+   var desc = document.getElementById("taskdesc").value;
+   var due = document.getElementById("duedate").value;
+   var priority = document.getElementById("priority").value; 
+   var title = document.getElementById('workoutdesc').value;
+   var removeoption = "<input type=button value=' X ' onClick='delRow()'>";
+    
+   if (desc === '')    // check if appropriate data are entered
+   {
+      document.getElementById("taskdesc").focus();
+      document.getElementById("taskdesc-note").innerHTML = "Please enter task description";
+   } 
+   else
+   {
+    document.getElementById("taskdesc-note").innerHTML = ""; 
+    
+      if (due === '' || isNaN((new Date(due)).valueOf()))
+      {        
+         document.getElementById("duedate").focus();
+         document.getElementById("duedate-note").innerHTML = 
+               "Date must be in <code>mm/dd/yyyy</code> or <code>mm/dd/yy</code> format and " + 
+            "month and date must be in appropriate ranges";
+      }
+      else
+      {
+         document.getElementById("duedate-note").innerHTML = "";
+    
+         // put all pieces of data in an array for later used to create cell content of a row 
+         var rowdata = [title, desc, due, priority, removeoption];
+   
+         // clear data entries (in the form)
+         document.getElementById('workoutdesc').value = "";
+         document.getElementById("taskdesc").value = "";
+         document.getElementById("duedate").value = "";
+         document.getElementById("priority").selectedIndex = "0";   
+         var tableRef = document.getElementById("todoTable");
+   
+         var newRow = tableRef.insertRow(tableRef.rows.length);    
+         newRow.onmouseover = function() { 
+
+            tableRef.clickedRowIndex = this.rowIndex;     
+         };    
+        
+         var newCell = "";       
+         var i = 0;
+            
+         while (i < 5)
+         {
+            newCell = newRow.insertCell(i);
+            newCell.innerHTML = rowdata[i];
+            newCell.onmouseover = this.rowIndex;
+            i++;
+         }
+      }
+   }
+}  
+
+function delRow()
+{
+   // since deletion action is unrecoverable, add hesitation to minimize/avoid user error 
+ if (confirm("Press OK to delete. This action is unrecoverable.") == true)   
+      document.getElementById("todoTable").deleteRow(document.getElementById("todoTable").clickedRowIndex);
+}
